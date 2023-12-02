e6b732180697e7e1f8312fb9d1fddcdcb4a1aa07
Mohamed_euler <96792977+mohamed-euler@users.noreply.github.com>
2023-12-03 01:10:37 +0300

    Update NFA.h
    update NFA.h to adapt last version of NFA.cpp

--- NFA.h
+++ NFA.h
@@ -1,47 +1,40 @@
 #ifndef NFA_H
 #define NFA_H
 
+#include <iostream>
 #include <vector>
 #include <map>
 #include <set>
-#include <unordered_map>
-#include <unordered_set>
 #include <string>
 
 using namespace std;
 
-class NFA
-{
+class NFA {
 private:
-    const char epsilon = '\0';                          // Define epsilon as an empty character
-    map<int, vector<pair<int, char>>> stateTransitions; // Map of state number to other states with input
-    map<int, vector<int>> epsilonTransitions;           // Epsilon transitions
-    set<char> alphabet;                                 // String alphabet
-    unordered_map<int, int> statePriorities;            // Map of state number to priority
-    unordered_map<int, string> priorityStrings;         // Map of priority number to the string
-    set<char> punctuations;                             // Set of punctuations
-    int numStates;                                      // Total number of states in the NFA
-    void updateNumStates(int from, int to);
+    map<int, vector<pair<int, char>>> stateTransitions;
+    map<int, vector<int>> epsilonTransitions;
+    set<char> alphabet;
+    map<int, int> statePriorities;
+    map<int, string> priorityStrings;
+    int startState = 0, endState = 0;
+    int priority = -1;
 
 public:
-    NFA();
+    void setPriority(int p);
+    void addSympol(char input);
     void addTransition(int from, int to, char input);
     void addEpsilonTransition(int from, int to);
-    void setStatePriority(int state, int priority);
-    void setPriorityString(int priority, const string &str);
-    void addPunctuation(char punctuation);
-    set<int> move(const set<int> &currentStates, char input);
-    set<int> epsilonClosure(const set<int> &states);
-    set<int> kleeneClosure(const set<int> &states);
-    set<int> positiveClosure(const set<int> &states);
-    void concatenate(NFA &other);
-    void Or(NFA &other);
-    set<int> setUnion(const set<int> &set1, const set<int> &set2);
+    static NFA positiveClosure(NFA &nfa1);
+    static NFA kleeneClosure(NFA &nfa1);
+    static NFA concatenate(NFA &nfa1, NFA &nfa2);
+    static NFA Or(NFA &nfa1, NFA &nfa2);
+    map<int, int> getStatePriorities() const;
     map<int, vector<pair<int, char>>> getStateTransitions() const;
+    map<int, vector<int>> getEpsilonTransitions() const;
     set<char> getAlphabet() const;
-    unordered_map<int, int> getStatePriorities() const;
-    unordered_map<int, string> getPriorityStrings() const;
-    set<char> getPunctuations() const;
+    const map<int, string> getPriorityStrings() const;
+    static NFA combine(vector<NFA> vec);
+    map<pair<int,char>,set<int>> getNfaTransitions() const;
 };
 
-#endif
+#endif // NFA_H
