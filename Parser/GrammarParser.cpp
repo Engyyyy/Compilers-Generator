380adf1e07e8394310231b5b3f11a8b9d8bc4d39
Engy <engeousthepotionmaster@gmail.com>
2023-12-24 02:11:25 +0200

    eliminate left recursion

--- Parser/GrammarParser.cpp
+++ Parser/GrammarParser.cpp
@@ -16,6 +16,84 @@ GrammarParser::GrammarParser(string inPath)
         terminals.push_back(terminal);
     for (auto nonTerminal : derivedNonTerminalsSet)
         nonTerminals.push_back(nonTerminal);
+
+    convertToPredictiveGrammar(PREDICTIVE_GRAMMAR_PROCEDURE::ELIMINATE_LEFT_RECURSION);
+}
+
+void GrammarParser::eliminateLeftRecursionStep(string Ai, vector<vector<string>> AiRules)
+{
+    // A -> Ab | Ac | d | f
+    // A -> dA' | fA'
+    // A' -> bA' | cA' | EPSILON
+    vector<vector<string>> leftRecursionRules, LL1Rules;
+    for (auto &AiRule : AiRules)
+    {
+        if (Ai == AiRule[0])
+        { // left recursion
+            leftRecursionRules.push_back(AiRule);
+        }
+        else
+        {
+            LL1Rules.push_back(AiRule);
+        }
+    }
+    if (leftRecursionRules.empty())
+        return;
+
+    string newNonTerminal = Ai + "\'";
+    vector<vector<string>> newNonTerminalRules;
+
+    for (auto &rule : LL1Rules)
+    {
+        rule.push_back(newNonTerminal);
+    }
+    productions[Ai] = LL1Rules;
+
+    for (auto &rule : leftRecursionRules)
+    {
+        vector<string> newRule;
+        newRule.insert(newRule.end(), rule.begin() + 1, rule.end());
+        newRule.push_back(newNonTerminal);
+        newNonTerminalRules.push_back(newRule);
+    }
+    vector<string> epsilonRule = {""};
+    newNonTerminalRules.push_back(epsilonRule);
+    productions[newNonTerminal] = newNonTerminalRules;
+}
+
+void GrammarParser::leftFactorStep(string Ai, vector<vector<string>> AiRules)
+{
+}
+
+void GrammarParser::convertToPredictiveGrammar(PREDICTIVE_GRAMMAR_PROCEDURE procedure)
+{
+    for (int i = 0; i < nonTerminals.size(); i++)
+    {
+        string Ai = nonTerminals[i];
+        vector<vector<string>> AiRules = productions[Ai];
+        for (int j = 0; j < i; j++)
+        {
+            string Aj = nonTerminals[j];
+            for (int k = 0; k < AiRules.size(); k++)
+            {
+                vector<string> &AiRule = AiRules[k];
+                if (AiRule[0] == Aj)
+                {
+                    vector<vector<string>> substitution = productions[Aj];
+                    for (auto &rule : substitution)
+                    {
+                        rule.insert(rule.end(), AiRule.begin() + 1, AiRule.end());
+                    }
+                    AiRule = substitution[0];
+                    AiRules.insert(AiRules.end(), substitution.begin() + 1, substitution.end());
+                }
+            }
+        }
+        if (procedure == ELIMINATE_LEFT_RECURSION)
+            eliminateLeftRecursionStep(Ai, AiRules);
+        else if (procedure == LEFT_FACTOR)
+            leftFactorStep(Ai, AiRules);
+    }
 }
 
 bool GrammarParser::verifyCompleteGrammar()
@@ -34,20 +112,19 @@ void GrammarParser::parse(string inPath)
 {
     ifstream inFile(inPath);
     string token;
-    string currNonTerminal, rule;
-    bool hasRule = false;
+    string currNonTerminal;
+    vector<string> rule;
     bool expectRule = false;
-    vector<string> rules;
+    vector<vector<string>> rules;
     while (inFile >> token)
     {
         if (token == "#")
         { // new production
             assert(!expectRule);
-            if (hasRule)
+            if (!rule.empty())
             {
                 rules.push_back(rule);
-                rule = "";
-                hasRule = false;
+                rule.clear();
             }
             if (!currNonTerminal.empty() && !rules.empty())
             {
@@ -77,56 +154,47 @@ void GrammarParser::parse(string inPath)
         {
             assert(!currNonTerminal.empty());
             assert(!expectRule);
-            assert(hasRule);
+            assert(!rule.empty());
             rules.push_back(rule);
-            rule = "";
-            hasRule = false;
+            rule.clear();
             expectRule = true;
         }
         else if (token == "\\L")
         {
             assert(!currNonTerminal.empty());
-            hasRule = true;
+            rule.push_back("");
             expectRule = false;
         }
         else if (token[0] == '\\')
         {
             assert(!currNonTerminal.empty());
             assert(token.size() > 1);
-            if (!rule.empty())
-                rule += " ";
-            rule += token.substr(1);
-            hasRule = true;
+            rule.push_back(token.substr(1));
             expectRule = false;
         }
         else
         {
             assert(!currNonTerminal.empty());
-            if (!rule.empty())
-                rule += " ";
-            hasRule = true;
             expectRule = false;
-
             if (isTerminal(token))
             {
                 token = token.substr(1, token.size() - 2);
                 terminalsSet.insert(token);
-                rule += token;
+                rule.push_back(token);
             }
             else
             {
                 usedNonTerminalsSet.insert(token);
-                rule += token;
+                rule.push_back(token);
             }
         }
     }
 
     assert(!expectRule);
-    if (hasRule)
+    if (!rule.empty())
     {
         rules.push_back(rule);
-        rule = "";
-        hasRule = false;
+        rule.clear();
     }
     if (!currNonTerminal.empty() && !rules.empty())
     {
@@ -161,7 +229,7 @@ string GrammarParser::getStartSymbol()
     return startSymbol;
 }
 
-map<string, vector<string>> GrammarParser::getProductions()
+map<string, vector<vector<string>>> GrammarParser::getProductions()
 {
     return productions;
 }
@@ -174,10 +242,12 @@ int main()
         cout << p.first << " -> ";
         for (auto rule : p.second)
         {
-            if (rule.empty())
-                cout << "EPSILON";
-            else
-                cout << rule;
+            for (auto token : rule)
+            {
+                if (token.empty())
+                    cout << "EPOSILON";
+                cout << token << " ";
+            }
             cout << " | ";
         }
         cout << endl;
--- Parser/GrammarParser.h
+++ Parser/GrammarParser.h
@@ -13,20 +13,28 @@ namespace Parser
     class GrammarParser
     {
     private:
+        enum PREDICTIVE_GRAMMAR_PROCEDURE
+        {
+            ELIMINATE_LEFT_RECURSION,
+            LEFT_FACTOR
+        };
         vector<string> terminals;
         vector<string> nonTerminals;
-        map<string, vector<string>> productions;
+        map<string, vector<vector<string>>> productions;
         string startSymbol;
         set<string> terminalsSet, derivedNonTerminalsSet, usedNonTerminalsSet;
         void parse(string inPath);
         bool isTerminal(string token);
         bool verifyCompleteGrammar();
+        void convertToPredictiveGrammar(PREDICTIVE_GRAMMAR_PROCEDURE procedure);
+        void eliminateLeftRecursionStep(string Ai, vector<vector<string>> AiRules);
+        void leftFactorStep(string Ai, vector<vector<string>> AiRules);
 
     public:
         vector<string> getTerminals();
         vector<string> getNonTerminals();
         string getStartSymbol();
-        map<string, vector<string>> getProductions();
+        map<string, vector<vector<string>>> getProductions();
         GrammarParser(string inPath);
         void parse();
     };
